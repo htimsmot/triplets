ed790acdf4cf6dda24d55b03b1f0357ac3e96938
htimsmot <132993711+htimsmot@users.noreply.github.com>
2023-07-05 07:23:38 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// alert("pow!");
-
-var selected = Math.floor(1 + Math.random() * 7);
-
-document
-  .querySelector(".img1")
-  .setAttribute("src", "./images/image" + selected + ".png");
-
-// "./images/image" + selected + ".png"
